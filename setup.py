7c1eae1b25727c30d880f1b8185a133085fc4ea6
安藤亮介 <crawd4274@gmail.com>
2017-08-11 15:32:03 +0900

    first commit

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup, find_packages
 from pip_github_test import __author__, __version__, __license__
 
 setup(
-    name='pip_github_test',
+    name='easy_mongo',
     version=__version__,
-    description='Sample for installing python library from github using pip',
+    description='easy mongodb',
     license=__license__,
     author=__author__,
-    author_email='junion@junion.org',
-    url='https://github.com/junion-org/pip_github_test.git',
+    author_email='crawd4274@gmail.com',
+    url='https://github.com/takashiAg/easy_mongo_python',
     keywords='sample pip github python',
     packages=find_packages(),
     install_requires=[],
